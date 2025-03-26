db55a12916ec13f41e11caceabfceb36f1e65767
dragssine <ouchen606@gmail.com>
2025-03-26 20:00:31 +0000

    Remove some files

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    // This prevents the error with Konva by telling webpack
-    // to use the browser version of Konva instead of the Node.js version
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'konva': 'konva/konva',
-    };
-    
-    return config;
-  },
   images: {
     remotePatterns: [
       {
